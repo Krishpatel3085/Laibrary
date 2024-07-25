4bfe54bea2b28eebeab44c77f165b0880b89dc4d
Krishpatel3085 <f388krish@gmail.com>
2024-07-25 17:07:33 +0530

    change

--- admin Panel/src/Pages/Books.jsx
+++ admin Panel/src/Pages/Books.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
 import { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -18,50 +18,60 @@ export default function Book() {
       });
   }, []);
 
-  // const deleteItem = async (id) => {
-  //   try {
-  //     await axios.delete(http://localhost:5000/items/${id});
-  //     fetchItems();
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
 
   return (
-    <div className="py-2 px-2 ">
+    <div className=" container py-2   ">
       <h1>Books</h1>
-
       <br />
+      <div className="mas d-flex px-5 ms-2 gap-5">
+        {items.map((item, index) => {
+          return (
+            <>
+              <div className="card book-card ">
+                <div className="row no-gutters">
+                  <div className="col-md-4">
+                    <img src={'https://ldfs6814-8085.inc1.devtunnels.ms/upload/' + item.url} className="img-fluid" />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title"><strong>Book Title: </strong>{item.title}</h5>
+                      <p className="card-text">
+                        <strong>Author:</strong>  {item.author}
+                      </p>
+                      <p className="card-text">
+                        <strong>Year:</strong>{item.year}
+                      </p>
+                      <p className="card-text">
+                        <strong>Isbin:</strong> {item.isbin}
+                      </p>
+                      <p className="card-text ">
+                        <strong>Summary:</strong> {item.description}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
 
-      <div className="conatainer Books">
-        <div className="row">
-          <div className="col-md-12 d-flex gap-3 flex-wrap">
-            {items.map((item, index) => {
-              return (
-                <Card style={{ width: "18rem", height:"650px" }} key={index} className="p-2">
-                  <Card.Img variant="top" src={'https://ldfs6814-8085.inc1.devtunnels.ms/upload/'+item.url} style={{width:"270px" , height:"300px"}} />
-                  <Card.Body>
-                    <Card.Title>{item.title}</Card.Title>
-                  </Card.Body>
-                  <ListGroup className="list-group-flush">
-                    <ListGroup.Item>{item.author}</ListGroup.Item>
-                    <ListGroup.Item>{item.year}</ListGroup.Item>
-                    <ListGroup.Item>{item.isbin}</ListGroup.Item>
-                    <ListGroup.Item>{item.language}</ListGroup.Item>
-                  </ListGroup>
-                    <Card.Text className="h-25 overflow-hidden">{item.description}</Card.Text> 
-                  <Card.Body>
-                    <Card.Link href="#">Edit</Card.Link>
-                    <Card.Link href="#">
-                      Delete
-                    </Card.Link>
-                  </Card.Body>
-                </Card>
-              );
-            })}
-          </div>
-        </div>
       </div>
     </div>
   );
 }
+
+// <div className="col-md-12">
+//   <div className=" col-md-5 ">
+//     <img src={'https://ldfs6814-8085.inc1.devtunnels.ms/upload/' + item.url} alt="" />
+//   </div>
+//   <div className="col-md-7">
+//     <h3>{item.title}</h3>
+//     <p>{item.author}</p>
+//     <p>{item.year}</p>
+//     <p>{item.isbin}</p>
+//     <p>{item.language}</p>
+//     <p>{item.description}</p>
+//     <button>Edit</button>
+//     <button>Delete</button>
+//   </div>
+// </div>
