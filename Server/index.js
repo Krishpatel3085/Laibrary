e03473b1fd49257874526cc638e19c851dc933da
Krish Kaila <145528945+Krishpatel3085@users.noreply.github.com>
2024-07-30 15:25:34 +0530

    Add Env file

--- Server/index.js
+++ Server/index.js
@@ -3,14 +3,15 @@ const mongoose = require("mongoose");
 const { book_routes } = require("./src/Routes/Book");
 const cors = require("cors");
 
+require('dotenv').config()
 
 const app = express();
 app.use(express.json())
 app.use(cors());
 app.use('/', book_routes)
 
-app.listen(8085, async () => {
-    await mongoose.connect("mongodb://localhost:27017/Book-Details")
+app.listen(process.env.PORT, async () => {
+    await mongoose.connect(process.env.MONGO_URL)
     console.log("Connected to MongoDB Succefully")
-    console.log("server Started on http://localhost:8085/")
+    console.log(`"server Started on http//localhost:${process.env.PORT}"`)
 }) 
