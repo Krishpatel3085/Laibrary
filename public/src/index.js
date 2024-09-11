e88e2e01582a3a472263c219980bcb80a1e16a59
Krishpatel3085 <f388krish@gmail.com>
2024-09-11 20:22:43 +0530

    Small Changes

--- public/src/index.js
+++ public/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-// use animation library
 import 'aos/dist/aos.css'; // Import AOS styles
 import AOS from 'aos'; // Import AOS library
 
