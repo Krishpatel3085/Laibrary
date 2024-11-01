a9a4f4ef612107a50883884fb01df1f420572834
Krish Kaila <145528945+Krishpatel3085@users.noreply.github.com>
2024-11-01 19:44:17 +0530

    add GetPayment Method

--- Server/src/Routes/payment.js
+++ Server/src/Routes/payment.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { verifyPayment, createPayorder } = require('../Controlers/payment');
+const { verifyPayment, createPayorder, getAllPayment } = require('../Controlers/payment');
 const PaymentRouter = express.Router();
 
 
 PaymentRouter.post('/createPayment', createPayorder);
 PaymentRouter.post('/verifyPayment', verifyPayment);
+PaymentRouter.get('/getPayment', getAllPayment);
 
 
 module.exports = { PaymentRouter };
