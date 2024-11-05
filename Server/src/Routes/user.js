47d4220ca7edcaf1942e6de4159429b8de0d17b2
Krish Kaila <145528945+Krishpatel3085@users.noreply.github.com>
2024-11-05 17:57:48 +0530

    Update User controller , & Adress schema

--- Server/src/Routes/user.js
+++ Server/src/Routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { getUSer, loginUser, createUser} = require ("../Controlers/user")
+const { getUSer, loginUser, createUser, get1USer } = require("../Controlers/user")
 
 const user_router = express.Router()
 
 user_router.get('/get', getUSer)
+user_router.get('/get1/:id', get1USer)
 user_router.post('/register', createUser)
 user_router.post('/login', loginUser)
 
-module.exports = {user_router};
+module.exports = { user_router };
